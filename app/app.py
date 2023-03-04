187b8ffcb967e06ae2e370cccc74e701bb7fc5b8
Mark Porter <mark.porter@gbatteries.com>
2023-03-04 04:13:32 -0500

    both apps need a backend

--- app/app.py
+++ app/app.py
@@ -9,20 +9,26 @@ time.sleep(20)
 print('Application started')
 
 app = Celery(
-    'postman',
+    'tasks',
     broker='pyamqp://user:bitnami@rabbitmq',
-    backend='rpc://user:bitnami@rabbitmq',
+    backend='redis://redis',
 )
 
-numTasks = 100
+numTasks = 10
 tasks = []
 
 for i in range(numTasks):
-    time.sleep(2 * random.random())  # Random delay
-    tasks.append(
-        app.send_task('addTask', (i, 3))  # Send task by name
-    )
+    time.sleep(0.5)  #  delay
+    
+    t = app.send_task('add', (i, 3))  # Send task by name
+
     print('Sent task:', i)
+    print(t)
+    print(t.status)
+    print(t.state)
+    print('Result', t.get())
+print('Finished tasks')
+print(len(tasks))
 
 for task in tasks:
     result = task.get()
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,8 +1,9 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery('tasks', broker='pyamqp://user:bitnami@rabbitmq//',
+             backend='redis://redis')
 
 
-@app.task
+@app.task(name='add')
 def add(x, y):
     return x + y
